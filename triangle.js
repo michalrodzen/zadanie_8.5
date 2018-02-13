995b278e7e6a49a061b669bd7a75041a5cbc7757
Michal Rodzen <michalrodzen1994@gmail.com>
2018-02-13 22:19:03 +0100

    Add triangle.js file

---
+++ triangle.js
@@ -0,0 +1,8 @@
+var a = prompt('podaj dłuość podstawy w [cm]'),
+h = prompt('podaj wysokość w [cm]'),
+triangleArea;
+
+triangleArea = a*h/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea + '[cm]');
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea + '[cm]');
